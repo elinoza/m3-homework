5a82db63eda2fe58554b6e0ccf2eff4881bf419b
elinoza <hillcakmak@gmail.com>
2020-11-02 20:26:24 +0300

    Update d1.js

--- d1.js
+++ d1.js
@@ -25,6 +25,20 @@ const newStr = function (str, y) {
   return res;
 };
 
+
+///from Mirela
+///function removeCharacter(str, num) {
+    //let str1 = str.substr(0,num)
+    //let str2 = str.substr(num)
+    ///console.log(str.substr(0,num -1).concat(str.substr(num)))
+
+    // let str = 'Im here'
+    // let toArray = str.split("")//[I, m, h, e, r,e]
+    // toArray.splice(1,2)
+    // let toStrBack = toArray.join("")
+    //console.log();
+
+
 /*
 4)
  Create a function to find the largest of three given integers.
@@ -106,7 +120,7 @@ return result
 
 
 /*
-10)
+10)Use of indexOf(1)
 Create a function to test whether an array of integers of length 2 does not contain 1 or a 3
 */
 
@@ -204,22 +218,19 @@ function findthelargest(myArray) {
   return  largest
 }
 
-let myArray = [ 1, 5, 9,60,43,21,79,50,45,64,23,24 ]; 
+/*let myArray = [ 1, 5, 9,60,43,21,79,50,45,64,23,24 ]; 
 console.log(findthelargest(myArray))
 
    /*
 15)
 Create a function to check two given numbers and return true if one of the number is 50 or if their sum is 50.
 */
-const calc = function (x, y) {
-    let result = x > 50 || y > 50 || x + y == 50 ? true : false;
-    return result;
-  };
+
    /*
 16)
 Create a function to check from two given integers, whether one is positive and another one is negative.
 */
-const calc = function (x, y) {
+const posAndNeg = function (x, y) {
     let result = x > 0 && y < 0 ||  x < 0 &&  y > 0 ? true : false;
     return result;
   };
@@ -229,26 +240,64 @@ const calc = function (x, y) {
 Create a function to create new string with first 3 characters
  are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case. 
 */
+
+
+  const newStr2 = function (str) {
+
+    if (str.length>2){
+        let first = str.substr(0,3).toLowerCase()
+        let second=str.substr(3).toUpperCase()
+        
+        return first.concat(second)
+    }
+    else {
+    
+     return  str.toUpperCase()
+    }
+    
+  }
+
+  
 /*
 18)
 Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
 */
+const ifRangeCalc = function (x, y) {
+    let result =80 > (x+y) && (x+y)> 50 ? 65 : 80;
+    return result;
+  }
 /*
 19)
 Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
-*/
-/*
+
 If the number has 3 as a factor, output 'Diego'.
 If the number has 5 as a factor, output 'Riccardo'.
 If the number has 7 as a factor, output 'Stefano'.
 If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
 Examples
-28's factors are 1, 2, 4, 7, 14, 28.
+28's factors çarpan) are 1, 2, 4, 7, 14, 28.
 this would be a simple "Stefano".
 30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
 this would be a "DiegoRiccardo".
 34 has four factors: 1, 2, 17, and 34.
 this would be "34".
+*/
+
+
+/*
 20)
 Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
-*/
+*/function makeAcronym(string) {
+    let acronym=[]
+    
+   let array=string.split(" ")
+   for(let i=0;i<array.length ;i++) {
+    let lettersArray=array[i].split("")
+    acronym.push(lettersArray[0])
+}
+   
+   
+    
+  return  acronym.join("")
+}
+
